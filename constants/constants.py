19bf886ae0603c88530f8942ce2a62c148d54b31
Rob <brucker.rob@gmail.com>
2024-12-30 14:45:34 -0500

    Initial commit
    Initial commit

---
+++ constants/constants.py
@@ -0,0 +1,35 @@
+class Constants:
+
+    def __init__(self):
+        self.skills = [
+            # Programming Languages
+            "Java", "Python", "Ruby", "JavaScript", "Kotlin", "PHP", "TypeScript",
+
+            # Frameworks
+            "Spring", "Node.js", "Rails", "Django", "Laravel", "Vue.js", "Angular",
+
+            # Databases and Data Tools
+            "PostgreSQL", "MySQL", "SQL Server", "SQLite", "Redshift", "MongoDB", "ElasticSearch", "Redis",
+
+            # Cloud Platforms
+            "AWS", "Azure", "Google Cloud Platform (GCP)",
+
+            # Professional Expertise
+            "API Development and Integration",
+            "Backend Development",
+            "Frontend Development",
+            "System Design and Scalability",
+            "Testing and Documentation",
+            "Data Analysis and Reporting",
+            "Team Leadership",
+            "CI/CD Systems",
+
+            # Soft Skills
+            "Technical Analysis and Debugging",
+            "Project Management and Collaboration",
+            "Stakeholder Communication"
+        ]
+
+        self.personal_name = "Rob Brucker"
+
+        self.model = "gpt-3.5-turbo"
